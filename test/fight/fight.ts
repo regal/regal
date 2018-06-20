24f3b85a5818d4254c0b6e8859296904003c8008
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-06-20 07:29:12 -0700

    Plan 2

--- test/fight/fight.ts
+++ test/fight/fight.ts
@@ -19,3 +19,39 @@ import {EventFunction, track} from '../../src/event';
 // 7 FALL[orc] - queued by 5
 // 8 LAND[item] - queued by 6
 // 9 LAND[orc] - queued by 7
+
+//** 1 INPUT */
+// Args: input (string)
+// Pipe to ATTACK
+
+//** 2 ATTACK */
+// Args: attacker (ICanAttack), target (IAttackable), object (IAttackObject)
+// Pipe to HIT
+
+//** 3 HIT */
+// Args: source (ICanHit), target (ITargetable), object (IHitObject)
+// Pipe to DAMAGE
+
+//** 4 DAMAGE */
+// Args: subject (IDamageable), damage (int)
+// Pipe to DEATH
+
+//** 5 DEATH */
+// Args: subject (ICanDie)
+// Queue DROP[item], FALL[orc]
+
+//** 6 DROP[item] */
+// Args: subject (ICanDrop), target (IDroppable)
+// Queue LAND[item]
+
+//** 7 FALL[orc] */
+// Args: subject (ICanFall)
+// Queue LAND[orc]
+
+//** 8 LAND[item] */
+// Args: subject (ICanFall)
+// END
+
+//** 9 Land[orc] */
+// Args: subject (ICanFall)
+// END
