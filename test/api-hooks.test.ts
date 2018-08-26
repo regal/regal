2d1d83729651bf9136b5d10d478a8739541b726b
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-08-26 15:57:50 -0500

    test(api): Tests for onStartCommand hook

--- test/api-hooks.test.ts
+++ test/api-hooks.test.ts
@@ -79,7 +79,7 @@ describe("API Hooks", function() {
             expect(myGame.state.input).to.equal("Test Command");
         });
     
-        it("Using onPlayerCommand with a TrackedEvent 'X' creates a TrackedEvent called INPUT that causes X.", function() {
+        it("Using onPlayerCommand with a TrackedEvent 'X' creates a TrackedEvent called INPUT that causes X", function() {
             const setInput = (command: string) => 
                 on("SET INPUT", game => {
                     game.state.input = command;
@@ -189,6 +189,196 @@ describe("API Hooks", function() {
 
         it("onPlayerCommand's handler must be defined", function() {
             expect(() => onPlayerCommand(undefined)).to.throw(RegalError, "Handler must be defined.");
+        });
+    });
+
+    describe("onStartCommand", function() {
+
+        it("onStartCommand allows the developer to set how start is handled (using an EventFunction)", function() {
+            onStartCommand(game => {
+                game.state.init = true;
+                return noop;
+            });
+
+            const myGame = new GameInstance();
+            HookManager.startCommandHook(myGame);
+
+            expect(myGame.state.init).to.be.true;
+        });
+
+        it("using onStartCommand with an EventFunction creates a TrackedEvent called START", function() {
+            onStartCommand(game => {
+                game.state.init = true;
+                return noop;
+            });
+
+            const myGame = new GameInstance();
+            HookManager.startCommandHook(myGame);
+
+            expect(myGame.events.history).to.deep.equal([
+                {
+                    id: 1,
+                    name: "START",
+                    changes: [{
+                        agentId: 0,
+                        op: PropertyOperation.ADDED,
+                        property: "init",
+                        init: undefined,
+                        final: true
+                    }]
+                }
+            ]);
+        });
+
+        it("onStartCommand allows the developer to set how start is handled (using a TrackedEvent)", function() {
+            const setInit = on("SET INIT", game => {
+                game.state.init = true;
+                return noop;
+            });
+
+            onStartCommand(setInit);
+
+            const myGame = new GameInstance();
+            HookManager.startCommandHook(myGame);
+
+            expect(myGame.state.init).to.be.true;
+        });
+
+        it("Using onStartCommand with a TrackedEvent 'X' creates a TrackedEvent called START that causes X", function() {
+            const setInit = on("SET INIT", game => {
+                game.state.init = true;
+                return noop;
+            });
+
+            onStartCommand(setInit);
+
+            const myGame = new GameInstance();
+            HookManager.startCommandHook(myGame);
+
+            expect(myGame.events.history).to.deep.equal([
+                {
+                    id: 2,
+                    name: "SET INIT",
+                    changes: [{
+                        agentId: 0,
+                        op: PropertyOperation.ADDED,
+                        property: "init",
+                        init: undefined,
+                        final: true
+                    }],
+                    causedBy: 1
+                },
+                {
+                    id: 1,
+                    name: "START",
+                    caused: [ 2 ]
+                }
+            ]);
+        });
+
+        it("onStartCommand allows the developer to set how start is handled (using an EventQueue)", function() {
+            const setFoo = (val: string) => 
+                on("SET FOO", game => {
+                    game.state.foo = val;
+                    return noop;
+                });
+
+            const setInit = on("SET INIT", game => {
+                game.state.init = true;
+                return setFoo("One");
+            });
+
+            const appendFoo = (val: string) =>
+                on("APPEND FOO", game => {
+                    game.state.foo += val;
+                    return noop;
+                });
+
+            onStartCommand(setInit.thenq(appendFoo("Two")));
+
+            const myGame = new GameInstance();
+            HookManager.startCommandHook(myGame);
+
+            expect(myGame.state.init).to.be.true;
+            expect(myGame.state.foo).to.equal("OneTwo");
+        });
+
+        it("Using onStartCommand with an EventQueue creates a TrackedEvent called START that causes each event in the queue", function() {
+            const setFoo = (val: string) => 
+                on("SET FOO", game => {
+                    game.state.foo = val;
+                    return noop;
+                });
+
+            const setInit = on("SET INIT", game => {
+                game.state.init = true;
+                return setFoo("One");
+            });
+
+            const appendFoo = (val: string) =>
+                on("APPEND FOO", game => {
+                    game.state.foo += val;
+                    return noop;
+                });
+
+            onStartCommand(setInit.thenq(appendFoo("Two")));
+
+            const myGame = new GameInstance();
+            HookManager.startCommandHook(myGame);
+
+            expect(myGame.events.history).to.deep.equal([
+                {
+                    id: 3,
+                    name: "APPEND FOO",
+                    changes: [{
+                        agentId: 0,
+                        op: PropertyOperation.MODIFIED,
+                        property: "foo",
+                        init: "One",
+                        final: "OneTwo"
+                    }],
+                    causedBy: 1
+                },
+                {
+                    id: 4,
+                    name: "SET FOO",
+                    changes: [{
+                        agentId: 0,
+                        op: PropertyOperation.ADDED,
+                        property: "foo",
+                        init: undefined,
+                        final: "One"
+                    }],
+                    causedBy: 2
+                },
+                {
+                    id: 2,
+                    name: "SET INIT",
+                    changes: [{
+                        agentId: 0,
+                        op: PropertyOperation.ADDED,
+                        property: "init",
+                        init: undefined,
+                        final: true
+                    }],
+                    causedBy: 1,
+                    caused: [ 4 ]
+                },
+                {
+                    id: 1,
+                    name: "START",
+                    caused: [ 2, 3 ]
+                }
+            ]);
+        });
+
+        it("onStartCommand cannot be called more than once", function() {
+            onStartCommand(noop);
+            expect(() => onStartCommand(noop)).to.throw(RegalError, "Cannot call onStartCommand more than once.");
+        });
+
+        it("onStartCommand's handler must be defined", function() {
+            expect(() => onStartCommand(undefined)).to.throw(RegalError, "Handler must be defined.");
         });
     });
 });
