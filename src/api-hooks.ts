0c551294b7590a53f99162f80e2876325e42748c
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-08-26 14:17:05 -0500

    feat(api): Add API hooks onPlayerCommand, onStartCommand, and onBeforeUndoCommand

--- src/api-hooks.ts
+++ src/api-hooks.ts
@@ -1,21 +1,49 @@
-import { EventFunction } from "./event";
+import { EventFunction, TrackedEvent, on } from "./event";
 import GameInstance from "./game-instance";
+import { RegalError } from "./error";
 
-export const HookManager = {
-    // TODO
-};
+const returnTrue = (game: GameInstance) => true;
+
+export class HookManager {
+    
+    static playerCommandHook: (command: string) => TrackedEvent;
+    static startCommandHook: TrackedEvent;
+    static beforeUndoCommandHook: (game: GameInstance) => boolean = returnTrue;
+}
 
 export const onPlayerCommand = (handler: (command: string) => EventFunction): void => {
-    // TODO
-    throw new Error("Method not implemented.");
+    if (HookManager.playerCommandHook !== undefined) {
+        throw new RegalError("Cannot call onPlayerCommand more than once.");
+    }
+    if (handler === undefined) {
+        throw new RegalError("Handler must be defined.");
+    }
+
+    const trackedEvent = (cmd: string) => on("INPUT", handler(cmd));
+
+    HookManager.playerCommandHook = trackedEvent;
 };
 
 export const onStartCommand = (handler: EventFunction): void => {
-    // TODO
-    throw new Error("Method not implemented.");
+    if (HookManager.startCommandHook !== undefined) {
+        throw new RegalError("Cannot call onStartCommand more than once.");
+    }
+    if (handler === undefined) {
+        throw new RegalError("Handler must be defined.");
+    }
+
+    const trackedEvent = on("START", handler);
+
+    HookManager.startCommandHook = trackedEvent;
 };
 
 export const onBeforeUndoCommand = (handler: (game: GameInstance) => boolean): void => {
-    // TODO
-    throw new Error("Method not implemented.");
+    if (HookManager.beforeUndoCommandHook !== returnTrue) {
+        throw new RegalError("Cannot call onBeforeUndoCommand more than once.");
+    }
+    if (handler === undefined) {
+        throw new RegalError("Handler must be defined.");
+    }
+
+    HookManager.beforeUndoCommandHook = handler;
 };
