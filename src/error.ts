53d09d1d74d35b57628980c94c3becec4a3597a1
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-08-26 15:56:26 -0500

    refactor(error): Remove ErrorCode enum

--- src/error.ts
+++ src/error.ts
@@ -1,10 +1,3 @@
-export enum ErrorCode {
-    OK,
-    NOT_YET_IMPLEMENTED,
-    INVALID_INPUT,
-    INVALID_STATE
-}
-
 export class RegalError extends Error {
 
     constructor(message: string = "") {
