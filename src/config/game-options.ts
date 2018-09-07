da00a3d97fb3edb6140736b2a91b48a373f8e736
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-09-06 20:01:58 -0500

    fix(api): Improves GameOption validation

--- src/config/game-options.ts
+++ src/config/game-options.ts
@@ -1,3 +1,5 @@
+import { RegalError } from "../error";
+
 /**
  * Represents game options that are configurable by a Regal client.
  */
@@ -26,3 +28,48 @@ export const DEFAULT_GAME_OPTIONS: GameOptions = {
 };
 
 export const OPTION_KEYS = Object.keys(DEFAULT_GAME_OPTIONS);
+
+export const validateOptions = (options: Partial<GameOptions>): void => {
+    // Ensure no extraneous options were included.
+    Object.keys(options).forEach(key => {
+        if (!OPTION_KEYS.includes(key)) {
+            throw new RegalError(`Invalid option name <${key}>.`);
+        }
+    });
+
+    const checkTypeIfDefined = (
+        key: keyof GameOptions,
+        expectedType: string
+    ): void => {
+        const value = options[key];
+        const actualType = typeof value;
+
+        if (options[key] !== undefined) {
+            if (actualType !== expectedType) {
+                throw new RegalError(
+                    `The option <${key}> is of type <${actualType}>, must be of type <${expectedType}>.`
+                );
+            }
+        }
+    };
+
+    checkTypeIfDefined("debug", "boolean");
+
+    if (options.forbidChanges !== undefined) {
+        if (Array.isArray(options.forbidChanges)) {
+            options.forbidChanges.forEach(optionName => {
+                if (!OPTION_KEYS.includes(optionName)) {
+                    throw new RegalError(
+                        `The option <${optionName}> does not exist.`
+                    );
+                }
+            });
+        } else if (typeof options.forbidChanges !== "boolean") {
+            throw new RegalError(
+                `The option <forbidChanges> is of type <${typeof options.forbidChanges}>, must be of type <boolean> or <string[]>.`
+            );
+        }
+    }
+
+    checkTypeIfDefined("showMinor", "boolean");
+};
--- src/config/instance-options.ts
+++ src/config/instance-options.ts
@@ -1,6 +1,10 @@
 import { RegalError } from "../error";
 import GameInstance from "../game-instance";
-import { DEFAULT_GAME_OPTIONS, GameOptions, OPTION_KEYS } from "./game-options";
+import {
+    DEFAULT_GAME_OPTIONS,
+    GameOptions,
+    validateOptions
+} from "./game-options";
 
 const OPTION_OVERRIDES_PROXY_HANDLER = {
     set(
@@ -44,26 +48,9 @@ export class InstanceOptions implements GameOptions {
     constructor(public game: GameInstance, overrides: Partial<GameOptions>) {
         this.overrides = new Proxy(overrides, OPTION_OVERRIDES_PROXY_HANDLER);
 
-        const overrideKeys = Object.keys(overrides);
+        validateOptions(overrides);
 
-        overrideKeys.forEach(key => {
-            // Ensure no extraneous options were included.
-            if (!OPTION_KEYS.includes(key)) {
-                throw new RegalError(`Invalid option name <${key}>.`);
-            }
-
-            // Ensure that each overriden option has the correct type.
-            // TODO - this doesn't work well for nonprimitive options. Perhaps each one should be validated seperately.
-            if (typeof overrides[key] !== typeof DEFAULT_GAME_OPTIONS[key]) {
-                throw new RegalError(
-                    `The option <${key}> is of type <${typeof overrides[
-                        key
-                    ]}>, must be of type <${typeof DEFAULT_GAME_OPTIONS[key]}>.`
-                );
-            }
-
-            this[key] = overrides[key];
-        });
+        Object.keys(overrides).forEach(key => (this[key] = overrides[key]));
 
         return new Proxy(this, INSTANCE_OPTIONS_PROXY_HANDLER);
     }
