2dae219074f69e83b63ee26808d81a24d6187978
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-11-13 09:47:54 -0600

    build: Bump Prando dependency

--- src/random/func/generate-seed.ts
+++ src/random/func/generate-seed.ts
@@ -2,7 +2,4 @@ import Prando from "prando";
 
 const SEED_LENGTH = 10;
 
-export const generateSeed = () => {
-    const THIS_IS_FAILING = new Prando().nextString(SEED_LENGTH);
-    return undefined; // TODO
-};
+export const generateSeed = () => new Prando().nextString(SEED_LENGTH);
