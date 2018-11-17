b4f9973edfebd6f27e8b1cd51f68b616b3540855
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-11-17 12:50:24 -0600

    build(rollup): Updates rollup script to output to bundle library as cjs, esm, and umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,41 @@
-import typescript from "rollup-plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
+
 import pkg from "./package.json";
+
+const supressCircularImportWarnings = (message, defaultFunc) => {
+    if (message.code === "CIRCULAR_DEPENDENCY") {
+        return;
+    }
+    defaultFunc(message);
+}
 
 export default {
     input: "./src/index.ts",
     plugins: [
-        typescript()
+        typescript({
+            tsconfigOverride: {
+                compilerOptions: {
+                    module: "ES2015"
+                }
+            }
+        }),
+        resolve()
     ],
     output: [
         {
             file: pkg.main,
-            // format: "cjs"
+            format: "cjs"
+        },
+        {
+            file: pkg.module,
+            format: "esm"
+        },
+        {
+            file: pkg.browser,
+            format: "umd",
+            name: "Regal"
         }
-    ]
+    ],
+    onwarn: supressCircularImportWarnings
 }
