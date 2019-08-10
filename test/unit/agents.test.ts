e9027b8be3374e4755d4a6a318104f4222e884a2
jcowman2 <joe.cowman@huskers.unl.edu>
2019-08-09 22:49:18 -0500

    test(agents): Fix unit test

--- test/unit/agents.test.ts
+++ test/unit/agents.test.ts
@@ -1217,6 +1217,7 @@ describe("Agents", function() {
 
                 smartObjectEquals(myGame.agents, {
                     _pkProvider: TestProperty.REQUIRE_BUT_SKIP,
+                    _prototypeRegistry: TestProperty.REQUIRE_BUT_SKIP,
                     game: myGame,
                     [pk0.value()]: smartObj({
                         meta: testMeta(pk0),
