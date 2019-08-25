1a727cc73c6f5d2fef4144904b88e750f488f07d
jcowman2 <joe.cowman@huskers.unl.edu>
2019-08-24 22:24:09 -0500

    docs(agents): Add TS docs to members of agent-proto-keys.ts

--- src/agents/impl/prototype/agent-proto-keys.ts
+++ src/agents/impl/prototype/agent-proto-keys.ts
@@ -8,14 +8,22 @@
 import { buildPKProvider } from "../../../common";
 import { AgentProtoId } from "../../agent-meta";
 
+/** The set of reserved `Agent` prototype keys. */
 export const AGENT_PROTO_RESERVED_KEYS = {
+    /**
+     * The reserved proto key for `GameInstance.state`, which has
+     * no customizable prototype.
+     */
     INSTANCE_STATE: 100
 };
 
+/** Helper function to build the `AgenProtoId` PK provider. */
 export const buildAgentProtoPKProvider = () =>
     buildPKProvider<AgentProtoId>(AGENT_PROTO_RESERVED_KEYS);
 
+/** The `Agent` prototype PK provider for static agents. */
 export const STATIC_PROTO_PK_PROVIDER = buildAgentProtoPKProvider();
 
+/** Shorthand for the `AGENT_PROTO_RESERVED_KEYS.INSTANCE_STATE` PK. */
 export const getInstanceStateAgentProtoPK = () =>
     STATIC_PROTO_PK_PROVIDER.reserved(AGENT_PROTO_RESERVED_KEYS.INSTANCE_STATE);
